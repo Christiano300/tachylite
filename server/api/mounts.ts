9ab2d20056f6c2a8bb79bee7067a8ae4dbbbfc23
Christian_Patzl <christian.patzl@htlstp.at>
2026-03-25 19:11:15 +0100

    fix: vercel deployment 7

--- server/api/mounts.ts
+++ server/api/mounts.ts
@@ -1,12 +1,10 @@
-import { mountConfigSchema } from "../../shared/types";
-
-const key = "tl:mounts";
+import { mountConfigSchema, MOUNTS_CONFIG_KEY } from "../../shared/types";
 
 export default eventHandler({
   handler: async (event) => {
     if (event.method === "GET") {
       const storage = useStorage("tl");
-      const mounts = await storage.getItem(key);
+      const mounts = await storage.getItem(MOUNTS_CONFIG_KEY);
       console.log(mounts, !mounts);
       return mounts ?? {};
     } else if (event.method === "PUT") {
@@ -26,7 +24,7 @@ export default eventHandler({
       );
 
       const storage = useStorage("tl");
-      await storage.setItem(key, JSON.stringify(mounts), { allowOverwrite: true });
+      await storage.setItem(MOUNTS_CONFIG_KEY, JSON.stringify(mounts), { allowOverwrite: true });
       return { success: true };
     } else {
       return new Response("Method not allowed", { status: 405 });
